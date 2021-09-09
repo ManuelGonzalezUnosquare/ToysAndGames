320045b7ddd73880181ee30dc6f6cba68ea25198
Manuel Gonzalez <manuel.unosquare@gmail.com>
2021-09-09 17:01:39 -0600

    delete service in progress

--- web/src/app/companies/company-delete/company-delete/company-delete.component.ts
+++ web/src/app/companies/company-delete/company-delete/company-delete.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
-
+import { ActivatedRoute, Router } from '@angular/router';
 @Component({
   selector: 'app-company-delete',
   templateUrl: './company-delete.component.html',
   styleUrls: ['./company-delete.component.scss']
 })
 export class CompanyDeleteComponent implements OnInit {
-
-  constructor() { }
+  constructor(private activatedRoute: ActivatedRoute, private router: Router) { 
+    let guid = this.activatedRoute.snapshot.params.guid;
+    if(!guid){
+      this.router.navigate(['/companies']);
+    }
+  }
 
   ngOnInit(): void {
+  }
+  getCompanyByGuid():void{
+    
   }
 
 }
--- web/src/app/companies/company-delete/services/company-delete.service.ts
+++ web/src/app/companies/company-delete/services/company-delete.service.ts
@@ -1,9 +1,13 @@
 import { Injectable } from '@angular/core';
+import {RequestService} from 'src/app/core/services';
 
 @Injectable({
   providedIn: 'root'
 })
 export class CompanyDeleteService {
 
-  constructor() { }
+  constructor(private requestService: RequestService) { }
+
+
+
 }
